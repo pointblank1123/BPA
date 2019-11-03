e8061853c40bc94f849a9de40b228d77425a0b6b
Alex Putman <43959556+pointblank1123@users.noreply.github.com>
2019-11-03 15:46:23 -0700

    Add files via upload
    11-3-19 Signup and login pages fixed

--- MasterEdit/BPA/login.php
+++ MasterEdit/BPA/login.php
@@ -6,10 +6,10 @@
             <h3>Log In</h3>
         </div>
         <div class="row justify-content-center signupContainer">
-            <form action="POST">
-                <input class="signupSheet" type="text" placeholder="Email" size="40" required><br>
-                <input class="signupSheet" type="password" placeholder="Password" size="40" required><br>
-                <button class="signupSheet submitButton" type="submit">Log In</button><br>
+            <form action="php/loginSheet.php" method="POST">
+                <input class="signupSheet" type="text" placeholder="Email" size="40" name="userEmail" required><br>
+                <input class="signupSheet" type="password" placeholder="Password" size="40" name="userPwd" required><br>
+                <button class="signupSheet submitButton" type="submit" name="login">Log In</button><br>
             </form>
         </div>
     </div>
